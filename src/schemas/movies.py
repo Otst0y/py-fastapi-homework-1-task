39230059a8b1a37628d0dfbd861cbd444ccaa66c
Otstoy <otstoy.sxe@gmail.com>
2025-10-06 12:49:34 +0300

    chore: implement routers for list and detail movies

--- src/schemas/movies.py
+++ src/schemas/movies.py
@@ -1,1 +1,31 @@
-# Write your code here
+import datetime
+
+from pydantic import BaseModel, ConfigDict
+
+
+class MovieDetailResponseSchema(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+    id: int
+    name: str
+    date: datetime.date
+    score: int
+    genre: str
+    overview: str
+    crew: str
+    orig_title: str
+    status: str
+    orig_lang: str
+    budget: float | None = None
+    revenue: float | None = None
+    country: str
+
+
+class MovieListResponseSchema(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+    movies: list[MovieDetailResponseSchema]
+    prev_page: str | None
+    next_page: str | None
+    total_pages: int
+    total_items: int
